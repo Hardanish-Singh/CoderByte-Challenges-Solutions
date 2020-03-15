47095b1649fa7c5be16e91eea55120bfbf0cd05d
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-15 11:29:40 -0400

    Add files via upload

---
+++ counting_minutes_one.js
@@ -0,0 +1,92 @@
+/****************************************************************
+ *             CODERBYTE COUNTING MINUTES ONE CHALLENGE         *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function CountingMinutesI(str) take the str         *
+ * parameter being passed which will be two times               *
+ * (each properly formatted with a colon and am or pm)          *
+ * separated by a hyphen and return the total number of minutes *
+ * between the two times. The time will be in a 12 hour clock   *
+ * format. For example: if str is 9:00am-10:00am then the       *
+ * output should be 60. If str is 1:00pm-11:00am the output     *
+ * should be 1320.                                              *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "12:30pm-12:00am"                                   *
+ * Output 1: 690                                                *
+ *                                                              *
+ * Input 2: "1:23am-1:08am"                                     *
+ * Output 2: 1425                                               *
+ ***************************************************************/
+
+function CountingMinutesI(str) {
+  //VARIABLE DECLARATION
+  let dateString = str;
+  let splitDateStringAtHiphen = dateString.split("-");
+  let hours1;
+  let minutes1 = "";
+  let ampm1 = "";
+  let hours2;
+  let minutes2 = "";
+  let ampm2 = "";
+  //LOOP THROUGH SPLITTED STRING AT HIPEN("-"")
+  for (let i = 0; i < splitDateStringAtHiphen.length; i++) {
+    //SPLIT THE STRING AT COLON(":")
+    let splitDateStringAtColon = splitDateStringAtHiphen[i].split(":");
+    //LOGIC TO FETCH HOURS1, MINUTES1, AMPM1, HOURS2, MINUTES2 & AMPM2 VARIABLES
+    for (let j = 0; j < splitDateStringAtColon.length; j++) {
+      if (j == 0 && i == 0) {
+        hours1 = Number(splitDateStringAtColon[j]);
+      } else if (j == 0 && i == 1) {
+        hours2 = Number(splitDateStringAtColon[j]);
+      } else if (j == 1 && i == 0) {
+        for (let k = 0; k < splitDateStringAtColon[j].length; k++) {
+          if (k < 2) minutes1 += splitDateStringAtColon[j][k];
+          else ampm1 += splitDateStringAtColon[j][k];
+        }
+      } else if (j == 1 && i == 1) {
+        for (let k = 0; k < splitDateStringAtColon[j].length; k++) {
+          if (k < 2) minutes2 += splitDateStringAtColon[j][k];
+          else ampm2 += splitDateStringAtColon[j][k];
+        }
+      }
+    }
+  }
+  //CONVERT MINUTES1, MINUTES2 TO NUMBER DATA TYPES
+  minutes1 = Number(minutes1);
+  minutes2 = Number(minutes2);
+
+  //IF EITHER AMPM1 OR AMPM2 TIMEZONE IS "PM" & EITHER HOURS1 OR HOURS2 <= 11, THEN ADD 12 HOURS TO EITHER HOURS1 OR HOURS2
+  if (ampm1 === "pm" && hours1 <= 11) {
+    hours1 += 12;
+  }
+  //IF EITHER AMPM1 OR AMPM2 TIMEZONE IS "PM" & EITHER HOURS1 OR HOURS2 <= 11, THEN ADD 12 HOURS TO EITHER HOURS1 OR HOURS2
+  if (ampm2 === "pm" && hours2 <= 11) {
+    hours2 += 12;
+  }
+  //IF EITHER AMPM1 OR AMPM2 TIMEZONE IS "AM" & EITHER HOURS1 OR HOURS2 IS EQUAL TO 12, THEN MAKE EITHER HOURS1 OR HOURS2 TO 0
+  if (ampm1 === "am" && hours1 == 12) {
+    hours1 = 0;
+  }
+  //IF EITHER AMPM1 OR AMPM2 TIMEZONE IS "AM" & EITHER HOURS1 OR HOURS2 IS EQUAL TO 12, THEN MAKE EITHER HOURS1 OR HOURS2 TO 0
+  if (ampm2 === "am" && hours2 == 12) {
+    hours2 = 0;
+  }
+  //CONVERT BOTH MINUTES1 AND MINUTES
+  let minutesOneConverted = hours1 * 60 + minutes1;
+  let minutesTwoConverted = hours2 * 60 + minutes2;
+  //CONDITIONAL CONVERTED MINUTES CHECKING
+  if (minutesOneConverted > minutesTwoConverted) {
+    hours2 += 24;
+    let timeOne = hours2 * 60 + minutes2;
+    let timeTwo = hours1 * 60 + minutes1;
+    return timeOne - timeTwo;
+  } else {
+    let timeOne = hours2 * 60 + minutes2;
+    let timeTwo = hours1 * 60 + minutes1;
+    return timeOne - timeTwo;
+  }
+}
+
+// KEEP THIS FUNCTION CALL HERE
+console.log(CountingMinutesI(readline()));
