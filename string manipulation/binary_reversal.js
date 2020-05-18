97190c93221a62c7d3116782d36230bff35eb63a
Hardanish Singh <hardanishsingh1@gmail.com>
2020-05-18 11:13:04 -0400

    Binary Reversal

--- string manipulation/binary_reversal.js
+++ string manipulation/binary_reversal.js
@@ -26,10 +26,10 @@
  ***************************************************************/
 function BinaryReversal(str) {
     let paddedBinaryRepresentation = (+str).toString(2);
-    while((paddedBinaryRepresentation.length % String.fromCharCode(56)) != String.fromCharCode(48)) paddedBinaryRepresentation = String.fromCharCode(48) + paddedBinaryRepresentation;
+    while(paddedBinaryRepresentation.length % 8 !== 0) paddedBinaryRepresentation = String.fromCharCode(48) + paddedBinaryRepresentation;
     let reversePaddedBinaryRepresentation = new String("");
-    for(let i=paddedBinaryRepresentation.length-1; i>=String.fromCharCode(48); i--) reversePaddedBinaryRepresentation += paddedBinaryRepresentation[i];
-    return parseInt(reversePaddedBinaryRepresentation, String.fromCharCode(50));
+    for(let i=paddedBinaryRepresentation.length-1; i>=0; i--) reversePaddedBinaryRepresentation += paddedBinaryRepresentation[i];
+    return parseInt(reversePaddedBinaryRepresentation, 2);
 }
 
 // KEEP THIS FUNCTION CALL HERE
