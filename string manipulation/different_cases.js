eb02cb17908d565e83a397bba46c9a22a47a4ce7
Hardanish Singh <hardanishsingh1@gmail.com>
2020-05-24 16:34:09 -0400

    Different Cases

---
+++ string manipulation/different_cases.js
@@ -0,0 +1,35 @@
+/****************************************************************
+ *             CODERBYTE DIFFERENT CASES CHALLENGE              *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function DifferentCases(str) take the str parameter *
+ * being passed and return it in upper camel case format where  *
+ * the first letter of each word is capitalized. The string will*
+ * only contain letters and some combination of delimiter       *
+ * punctuation characters separating each word.                 *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "Daniel LikeS-coding"                               *
+ * Output 1: DanielLikesCoding                                  *
+ *                                                              *
+ * Input 2: "cats AND*Dogs-are Awesome"                         *
+ * Output 2: CatsAndDogsAreAwesome                              *
+ *                                                              *
+ * Input 3: "a b c d-e-f%g"                                     *
+ * Output 3: ABCDEFG                                            *
+ *                                                              *
+ * Solution Efficiency                                          *
+ * The user scored higher than 31.2% of users who solved this   *
+ * challenge.                                                   *
+ *                                                              *
+ ***************************************************************/
+
+function DifferentCases(string) { 
+    let array = string.match(/\w+|\s+|[^\s\w]+/g);
+    let differentCases = new String("");
+    for(let i=0; i<array.length; i++) if(array[i].match(/^[a-zA-Z]+$/)) differentCases += array[i][0].toUpperCase() + array[i].substr(1).toLowerCase();
+    return differentCases
+}
+    
+// KEEP THIS FUNCTION CALL HERE
+console.log(DifferentCases(readline()));
