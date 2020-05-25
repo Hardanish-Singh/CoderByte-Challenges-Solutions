a3f5fa3b23e97f0b1638f2a4c12e8caf00b355c5
Hardanish Singh <hardanishsingh1@gmail.com>
2020-05-25 08:46:04 -0400

    Palindrome

--- string manipulation/palindrome.js
+++ string manipulation/palindrome.js
@@ -26,8 +26,8 @@ function Palindrome(str) {
   let originalString = str.replace(/\s/g, "");
   let reversedString = "";
   for (let i = originalString.length - 1; i >= 0; i--) reversedString += originalString[i];
-  if (originalString === reversedString)  return "true";
-  else return "false";
+  if (originalString === reversedString)  return true;
+  else return false;
 }
 
 // KEEP THIS FUNCTION CALL HERE
