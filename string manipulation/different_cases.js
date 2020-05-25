3792fcbc1ac8e71136dc651ac8d95fbf78e79631
Hardanish Singh <hardanishsingh1@gmail.com>
2020-05-25 08:45:38 -0400

    Different Cases

--- string manipulation/different_cases.js
+++ string manipulation/different_cases.js
@@ -24,11 +24,12 @@
  *                                                              *
  ***************************************************************/
 
-function DifferentCases(string) { 
-    let array = string.match(/\w+|\s+|[^\s\w]+/g);
+function DifferentCases(string) {
+    let punctuationRegex = /\w+|\s+|[^\s\w]+/g;
+    let array = string.match(punctuationRegex); 
     let differentCases = new String("");
     for(let i=0; i<array.length; i++) if(array[i].match(/^[a-zA-Z]+$/)) differentCases += array[i][0].toUpperCase() + array[i].substr(1).toLowerCase();
-    return differentCases
+    return differentCases;
 }
     
 // KEEP THIS FUNCTION CALL HERE
