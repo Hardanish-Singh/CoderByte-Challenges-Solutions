750369ac1c9e03cff360c954eb35a152738be256
Hardanish Singh <hardanishsingh1@gmail.com>
2020-06-01 09:47:29 -0400

    Minimum Window Substring

--- string manipulation/minimum_window_substring.js
+++ string manipulation/minimum_window_substring.js
@@ -1,49 +1,37 @@
-function MinWindowSubstring(stringArray) {
+function MinWindowSubstring (stringArray) {
     let string = stringArray[0];
     let target = stringArray[1];
     let targetDictionary = {};
     let stringDictionary = {};
     for(let i=0; i<target.length; i++){
-        if(!(target[i] in targetDictionary)) targetDictionary[target[i]] = 1;
-        else targetDictionary[target[i]] =  targetDictionary[target[i]] + 1;
+        if(target[i] in targetDictionary) targetDictionary[target[i]] += 1;
+        else targetDictionary[target[i]] = 1;
     }
     let leftPointer = 0;
     let rightPointer = 0;
-    let subStringLength = 1000000000;
+    let subStringLength = 2147483647;
     let lp = 0;
     let rp = 0;
     while(true) {
-        if(leftPointer>rightPointer || rightPointer > string.length)break;
+        if(leftPointer > rightPointer || rightPointer > string.length) break;
         let flag = checkObjectKeysArray(Object.keys(targetDictionary), Object.keys(stringDictionary), targetDictionary, stringDictionary);
         if(flag === true) {
-            if(string[leftPointer] in stringDictionary) {
-                if(stringDictionary[string[leftPointer]] > targetDictionary[string[leftPointer]]) {
-                    stringDictionary[string[leftPointer]] = stringDictionary[string[leftPointer]] - 1;
-                    leftPointer++;
-                }
-                else {
-                    rightPointer--;
-                    if((rightPointer - leftPointer + 1) <= subStringLength){
-                        subStringLength = rightPointer - leftPointer + 1;
-                        lp = leftPointer;
-                        rp = rightPointer;
-                    }
-                    delete stringDictionary[string[leftPointer]];
-                    leftPointer++;
-                    rightPointer++;
-                }
+            if((rightPointer - leftPointer + 1) <= subStringLength){
+                subStringLength = rightPointer - leftPointer + 1;
+                lp = leftPointer;
+                rp = rightPointer;
             }
-            else leftPointer++;
+            if(string[leftPointer] in stringDictionary) stringDictionary[string[leftPointer]] -= 1;
+            leftPointer++;
         } 
         else {
-            if(!(string[rightPointer] in stringDictionary)) stringDictionary[string[rightPointer]] = 1;
-            else stringDictionary[string[rightPointer]] =  stringDictionary[string[rightPointer]] + 1;
+            if(string[rightPointer] in stringDictionary) stringDictionary[string[rightPointer]] += 1;
+            else stringDictionary[string[rightPointer]] = 1;
             rightPointer++; 
         }
     }
-    return string.substring(lp, rp+1);
-}
-
+    return string.substring(lp, rp);
+};
 function checkObjectKeysArray(array1, array2, targetDictionary, stringDictionary){
     if(array2.length===0 || array1.length > array2.length) return false;
     let temp = false;
@@ -59,6 +47,5 @@ function checkObjectKeysArray(array1, array2, targetDictionary, stringDictionary
     }
     return temp;
 }
-   
-// keep this function call here 
+// KEEP THIS FUNCTION CALL HERE
 console.log(MinWindowSubstring(readline()));
