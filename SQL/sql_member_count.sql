b85e8d4d87cfd90cb7907ff5d22d2c121880f66b
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-01 16:39:02 -0400

    SQL Member Count

--- SQL/sql_member_count.sql
+++ SQL/sql_member_count.sql
@@ -1,7 +1,7 @@
 /****************************************************************
  *              CODERBYTE SQL MEMBER COUNT CHALLENGE            *
  *                                                              *
- * Your table: maintable_BQH7X                                  *
+ * Your table: maintable_L99ON                                  *
  * MySQL version: 5.5.56-log                                    *
  *                                                              *
  * Problem Statement                                            *
@@ -14,20 +14,12 @@
  * order. Your output should look like the following table.     *
  *                                                              *
  ***************************************************************/
- 
+
 SELECT 
-        DISTINCT
-                t2.*
-FROM 
-        maintable_BQH7X AS t1
-JOIN 
-(
-        SELECT 
-                ReportsTo,
-                COUNT(ReportsTo) AS Members,
-                CEIL(AVG(AGE)) AS `Average Age`
-        FROM
-                maintable_BQH7X
-        WHERE ReportsTo IS NOT NULL
-        GROUP BY ReportsTo
-)AS t2
+        ReportsTo,
+        COUNT(ReportsTo) AS Members,
+        CEIL(AVG(AGE)) AS `Average Age`
+FROM
+        maintable_L99ON
+WHERE ReportsTo IS NOT NULL
+GROUP BY ReportsTo
