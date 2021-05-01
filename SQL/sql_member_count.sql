4988f824ab6cc1ea8485d67ff6c04f4685da879c
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-01 16:37:01 -0400

    SQL Member Count

--- SQL/sql_member_count.sql
+++ SQL/sql_member_count.sql
@@ -1,3 +1,20 @@
+/****************************************************************
+ *              CODERBYTE SQL MEMBER COUNT CHALLENGE            *
+ *                                                              *
+ * Your table: maintable_BQH7X                                  *
+ * MySQL version: 5.5.56-log                                    *
+ *                                                              *
+ * Problem Statement                                            *
+ * In this MySQL challenge, your query should return the names  *
+ * of the people who are reported to (excluding null values),   *
+ * the number of members that report to them, and the average   *
+ * age of those members as an integer.                          *
+ *                                                              *
+ * The rows should be ordered by the names in alphabetical      *
+ * order. Your output should look like the following table.     *
+ *                                                              *
+ ***************************************************************/
+ 
 SELECT 
         DISTINCT
                 t2.*
