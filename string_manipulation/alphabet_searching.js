1cad17c06960e3f1f7329ea82c409b5b49af8c9a
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-05 16:44:17 -0400

    Alphabet Searching

---
+++ string_manipulation/alphabet_searching.js
@@ -0,0 +1,44 @@
+/****************************************************************
+ *             CODERBYTE ALPHABET SEARCHING CHALLENGE           *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function AlphabetSearching(str) take the string     *
+ * parameter being passed and return the string true if every   *
+ * single letter of the English alphabet exists in the string,  *
+ * otherwise return the string false.                           *
+ * For example: if str is "zacxyjbbkfgtbhdaielqrm45pnsowtuv"    *
+ * then your program should return the string true because every*
+ * character in the alphabet exists in this string even though  *
+ * some characters appear more than once.                       *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: abcdefghijklmnopqrstuvwxyyyy                        *
+ * Output 1: false                                              *
+ *                                                              *
+ * Input 2: abc123456kmo                                        *
+ * Output 2: false                                              *
+ ***************************************************************/
+
+function AlphabetSearching(string) {
+  //SORT THE STRING & REMOVE DUPLICATES FROM THE STRING
+  let sortedStringArray = Array.from(new Set(string.split("").sort()));
+  //COUNT THE ENGLISH ALPHABET CHARACTERS
+  let count = 0;
+  //LOOP THROUGH STRING
+  for (let i = 0; i < sortedStringArray.length; i++) {
+    if (
+      sortedStringArray[i].toLowerCase() >= "a" &&
+      sortedStringArray[i].toLowerCase() <= "z"
+    ) {
+      count++;
+    }
+  }
+  if (count === 26) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// KEEP THIS FUNCTION CALL HERE
+console.log(AlphabetSearching(readline()));
