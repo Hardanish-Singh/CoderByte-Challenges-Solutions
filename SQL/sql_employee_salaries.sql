79417617626e282173473aef96da1fe235c30910
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-07 19:06:34 -0400

    SQL Employee Salaries

--- SQL/sql_employee_salaries.sql
+++ SQL/sql_employee_salaries.sql
@@ -21,7 +21,7 @@
  * Problem Statement                                                                         *
  * In this MySQL challenge, your query should return the information for the employee with   *
  * the third highest salary. Write a query that will find this employee and return that row, *
- * but then replace the DivisionID column with the corresponding DivisionName from the table * 
+ * but then replace the DivisionID column with the corresponding DivisionName from the table *
  * cb_companydivisions. You should also replace the ManagerID column with the ManagerName if *
  * the ID exists in the table and is not NULL.                                               *
  *                                                                                           *
