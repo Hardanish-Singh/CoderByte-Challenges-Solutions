9ae32719f56af80e244e7c69466886399bd5ff5f
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-07 06:46:15 -0400

    ASCII Convertion

--- string manipulation/ascii_convertion.js
+++ string manipulation/ascii_convertion.js
@@ -18,17 +18,18 @@
  ***************************************************************/
 
 function ASCIIConversion( str ) {
-  let asciiValueString = new String("");
+        let asciiValueString = new String("");
 
-  for ( let i = 0; i < str.length; i++ ) {
-    if ( str[i] === " " ) {
-      asciiValueString += str[i];
-    } else {
-      asciiValueString += str.charCodeAt(i);
-    }
-  }
+        for ( let i = 0; i < str.length; i++ ) {
+                if ( str[i] === " " ) {
+                        asciiValueString += str[i];
+                }
+                else {
+                        asciiValueString += str.charCodeAt(i);
+                }
+        }
 
-  return asciiValueString;
+        return asciiValueString;
 }
 
 // KEEP THIS FUNCTION CALL HERE
