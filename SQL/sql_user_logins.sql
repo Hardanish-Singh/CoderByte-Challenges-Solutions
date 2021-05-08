6ce504aa589d3d55420cb1ffb2d2d8883d73f146
Hardanish Singh <hardanishsingh1@gmail.com>
2021-05-08 12:20:50 -0400

    SQL User Logins

--- SQL/sql_user_logins.sql
+++ SQL/sql_user_logins.sql
@@ -1,3 +1,6 @@
+/*
+        SOLUTION ONE WITHOUT LAG FUNCTION
+*/
 SELECT
         MONTHNAME(STR_TO_DATE(MONTH(DateJoined), '%m')) AS Month,
         date1 - previous AS MonthToMonthChange
@@ -21,3 +24,29 @@ FROM
         )AS SubQuery1
 ) AS SubQuery2
 WHERE previous > 0
+
+/*
+        SOLUTION 2 WITH LAG FUNCTION
+*/
+
+SELECT
+        MONTHNAME(STR_TO_DATE(MONTH(DateJoined), '%m')) AS Month,
+        date1 - date2 AS MonthToMonthChange
+FROM
+(
+        SELECT
+                date1,
+                LAG(date1, 1) OVER ( )AS date2,
+                DateJoined
+        FROM 
+        (
+                SELECT 
+                        COUNT(DateJoined) AS date1,
+                        DateJoined
+                FROM 
+                        leetcode.user_login
+                GROUP BY MONTH(DateJoined)
+                ORDER BY DateJoined
+        )AS SubQuery1
+) AS SubQuery2
+WHERE SubQuery2.date2 IS NOT NULL;
