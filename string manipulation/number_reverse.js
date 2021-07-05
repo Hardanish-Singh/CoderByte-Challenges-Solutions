c1f6917c724251ccc015692b8dd7d5e7fe89c6dc
Hardanish Singh <hardanishsingh1@gmail.com>
2021-07-05 19:49:28 -0400

    Number Reverse

--- string manipulation/number_reverse.js
+++ string manipulation/number_reverse.js
@@ -1,3 +1,20 @@
+/****************************************************************
+ *             CODERBYTE NUMBER REVERSE CHALLENGE               *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function NumberReverse(str) take the str parameter	*
+ * being passed which will be a string of numbers, and return a *
+ * new string with the numbers in reverse order.  		*
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "1 2 3"                                             *
+ * Output 1: 3 2 1                                              *
+ *                                                              *
+ * Input 2: "10 20 50"                                          *
+ * Output 2: 50 20 10                                           *
+ *                                                              *
+ ***************************************************************/
+
 function NumberReverse( str ) {
 	let numbers = [];
 	let temp = '';
@@ -33,5 +50,5 @@ function NumberReverse( str ) {
 	return numberReverse.trim();
 }
 
-// KEEP THIS FUNCTION CALL HERE 
+// KEEP THIS FUNCTION CALL HERE
 console.log(NumberReverse(readline()));
