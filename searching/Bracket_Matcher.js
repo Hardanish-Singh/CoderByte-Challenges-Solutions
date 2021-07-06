a6f5f6aff704d818ce5abd412f593efeed5f03eb
Hardanish Singh <hardanishsingh1@gmail.com>
2021-07-06 05:25:31 -0400

    Bracket Matcher

--- searching/Bracket_Matcher.js
+++ searching/Bracket_Matcher.js
@@ -1,3 +1,25 @@
+/****************************************************************
+ *          CODERBYTE BRACKET MATCHER CHALLENGE		        *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function BracketMatcher(str) take the str parameter	*
+ * being passed and return 1 if the brackets are correctly	*
+ * matched and each one is accounted for. Otherwise return 0.	*
+ * For example: if str is "(hello (world))", then the output	*
+ * should be 1, but if str is "((hello (world))" the the output	*
+ * should be 0 because the brackets do not correctly match up.	*
+ * Only "(" and ")" will be used as brackets. If str contains	*
+ * no brackets return 1.    					*
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "(coder)(byte))"                                    *
+ * Output 1: 0                                                  *
+ *                                                              *
+ * Input 2: "(c(oder)) b(yte)"                                  *
+ * Output 2: 1                                                  *
+ *                                                              *
+ ***************************************************************/
+
 function BracketMatcher( str )
 {
 	let count = 0;
