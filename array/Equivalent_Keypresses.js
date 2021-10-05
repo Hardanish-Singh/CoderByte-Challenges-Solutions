430ab58af5f51abfac74a9d7a9346b2162257e70
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-05 08:18:57 -0400

    Equivalent Keypresses

--- array/Equivalent_Keypresses.js
+++ array/Equivalent_Keypresses.js
@@ -7,36 +7,36 @@ function EquivalentKeypresses(strArr) {
         let s2 = "";
       
         for( let i=0; i<string1.length; i++ ){
-          if( string1[i] === "-B") {
-            continue;
-          }
-          if( string1[i+1] !== "-B" ) {
-            s1 += string1[i];
-          }
-          else {
-            i++;
-          }
+                if( string1[i] === "-B") {
+                        continue;
+                }
+                if( string1[i+1] !== "-B" ) {
+                        s1 += string1[i];
+                }
+                else {
+                        i++;
+                }
         }
       
         for( let i=0; i<string2.length; i++ ){
-          if( string2[i] === "-B" ) {
-            continue;
-          }
-          if( string2[i+1] !== "-B" ) {
-            s2 += string2[i];
-          }
-          else {
-            i++;
-          }
+                if( string2[i] === "-B" ) {
+                        continue;
+                }
+                if( string2[i+1] !== "-B" ) {
+                        s2 += string2[i];
+                }
+                else {
+                        i++;
+                }
         }
       
         if( s1 === s2 ) {
-          return true;
+                return true;
         } 
         else {
-          return false;
+                return false;
         }
       }
          
-      // keep this function call here 
-      console.log(EquivalentKeypresses(readline()));
+// KEEP THIS FUNCTION CALL HERE
+console.log(EquivalentKeypresses(readline()));
