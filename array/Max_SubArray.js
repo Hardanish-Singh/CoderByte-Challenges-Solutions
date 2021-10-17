bf99cbd1136936de4572c7c8a4fa6ba1b20d46bb
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-17 09:45:41 -0400

    Max SubArray

--- array/Max_SubArray.js
+++ array/Max_SubArray.js
@@ -1,3 +1,24 @@
+/****************************************************************
+ *                 CODERBYTE MAX SUBARRAY CHALLENGE             *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function MaxSubarray(arr) take the array of numbers *
+ * stored in arr and determine the largest sum that can be      *
+ * formed by any contiguous subarray in the array.              *
+ * For example, if arr is [-2, 5, -1, 7, -3] then your program  *
+ * should return 11 because the sum is formed by the subarray   *
+ * [5, -1, 7]. Adding any element before or after this subarray *
+ * would make the sum smaller.                                  *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: [1, -2, 0, 3]                                       *
+ * Output 1: 3                                                  *
+ *                                                              *
+ * Input 2: [3, -1, -1, 4, 3, -1]                               *
+ * Output 2: 8                                                  *
+ *                                                              *
+ ***************************************************************/
+
 function MaxSubarray(nums) { 
 
         let sum = -100001;
