179cf6eeb1a93c39e606c11b628fbe412a641a2c
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-19 10:42:54 -0400

    Word Split

--- array/Word_Split.js
+++ array/Word_Split.js
@@ -32,7 +32,7 @@
 
 function getAllIndexes(arr, val) {
         let indexes = [], i = -1;
-        while ( ( i = arr.indexOf( val,  i+1 ) ) != -1 ){
+        while ( ( i = arr.indexOf( val,  i+1 ) ) != -1 ) {
             indexes.push(i);
         }
         return indexes;
