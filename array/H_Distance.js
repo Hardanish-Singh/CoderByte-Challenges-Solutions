c2b52df6d0529447964227e8eb52f49b49242403
Hardanish Singh <hardanishsingh1@gmail.com>
2021-10-27 08:27:01 -0400

    H_Distance

--- array/H_Distance.js
+++ array/H_Distance.js
@@ -1,3 +1,26 @@
+/****************************************************************
+ *                CODERBYTE H DISTANCE CHALLENGE                *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function HDistance(strArr) take the array of strings*
+ * stored in strArr, which will only contain two strings of     *
+ * equal length and return the number of characters at each     *
+ * position that are different between them.                    *
+ *                                                              *
+ * For example: if strArr is ["house", "hours"] then your       *
+ * program should return 2. The string will always be of equal  *
+ * length and will only contain lowercase characters from the   *
+ * alphabet and numbers.                                        *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: ["10011", "10100"]                                  *
+ * Output 1: 3                                                  *
+ *                                                              *
+ * Input 2: ["abcdef", "defabc"]                                *
+ * Output 2: 6                                                  *
+ *                                                              *
+ ***************************************************************/
+
 function HDistance( strArr ) { 
         let h_distance = 0;
         let string1 = strArr[0];
