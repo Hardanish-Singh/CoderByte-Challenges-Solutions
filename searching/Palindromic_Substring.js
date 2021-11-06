6ed5c397adc85c23b2381cb5bde0aaf01a7bb7c4
Hardanish Singh <hardanishsingh@Hardanishs-MacBook-Pro.local>
2021-11-05 20:59:50 -0400

    Palindromic SubString

--- searching/Palindromic_Substring.js
+++ searching/Palindromic_Substring.js
@@ -1,3 +1,29 @@
+/****************************************************************
+ *          CODERBYTE PALINDROMIC SUBSTRING CHALLENGE		*
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function PalindromicSubstring(str) take the str     *
+ * parameter being passed and find the longest palindromic      *
+ * substring, which means the longest substring which is read   *
+ * the same forwards as it is backwards.                        *
+ * For example: if str is "abracecars" then your program should *
+ * return the string racecar because it is the longest          *
+ * palindrome within the input string.                          *
+ *                                                              *
+ * The input will only contain lowercase alphabetic characters. *
+ * The longest palindromic substring will always be unique,     *
+ * but if there is none that is longer than 2 characters,       *
+ * return the string none.                                      *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "hellosannasmith                                    *
+ * Output 1: sannas                                             *
+ *                                                              *
+ * Input 2: "abcdefgg"                                          *
+ * Output 2: none                                               *
+ *                                                              *
+ ***************************************************************/
+
 function isPalindrome( string ) {
         let leftPointer = 0;
         let rightPointer = string.length - 1;
