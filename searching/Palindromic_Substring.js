330a5d62c153be70e256bd23446643490edadf07
Hardanish Singh <hardanishsingh@Hardanishs-MacBook-Pro.local>
2021-11-05 21:01:26 -0400

    Palindromic SubString

--- searching/Palindromic_Substring.js
+++ searching/Palindromic_Substring.js
@@ -27,41 +27,42 @@
 function isPalindrome( string ) {
         let leftPointer = 0;
         let rightPointer = string.length - 1;
-      
+
         while( leftPointer < rightPointer ) {
-          if( string[leftPointer] !== string[rightPointer] ) {
-            return false;
-          }
-          leftPointer++;
-          rightPointer--;
+                if( string[leftPointer] !== string[rightPointer] ) {
+                        return false;
+                }
+                leftPointer++;
+                rightPointer--;
         }
-      
+
         return true;
-      }
+}
       
-      function PalindromicSubstring( str ) {
+function PalindromicSubstring( str ) {
         let PalindromicSubstrings = [];
         for( let i=0; i<str.length; i++ ) {
-          let reverseStr = str.slice(i+1).split("").reverse().join("");
-          for( let j=0; j<reverseStr.length; j++ ) {
-            if( reverseStr.slice(j) === str[i] ) continue;
-            let subString =  ( reverseStr.slice(j) + str[i] ).split("").reverse().join("");
-            if( isPalindrome( subString ) ) {
-              PalindromicSubstrings.push( subString );
-            }
-          }
+                let reverseStr = str.slice(i+1).split("").reverse().join("");
+                for( let j=0; j<reverseStr.length; j++ ) {
+                        if( reverseStr.slice(j) === str[i] ) {
+                                continue;
+                        }
+                        let subString =  ( reverseStr.slice(j) + str[i] ).split("").reverse().join("");
+                        if( isPalindrome( subString ) ) {
+                                PalindromicSubstrings.push( subString );
+                        }
+                }
         }
         let max = -1;
         let index = -1;
-        for( let i=0; i<PalindromicSubstrings.length; i++ ){
-          if(PalindromicSubstrings[i].length > max) {
-            max = PalindromicSubstrings[i].length;
-            index = i;
-          }
+        for( let i=0; i<PalindromicSubstrings.length; i++ ) {
+                if(PalindromicSubstrings[i].length > max) {
+                        max = PalindromicSubstrings[i].length;
+                        index = i;
+                }
         }
-        // console.log(PalindromicSubstrings, max);
         return index === -1 ? "none" : PalindromicSubstrings[index];
-      }
+}
          
       // keep this function call here 
       console.log(PalindromicSubstring(readline()));
