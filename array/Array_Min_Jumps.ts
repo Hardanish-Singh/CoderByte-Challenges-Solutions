1d8abd6a57f495357db2d9522aeea14db49c33fd
Hardanish Singh <hardanishsingh1@gmail.com>
2021-11-08 07:21:00 -0500

    Array Min Jumps

--- array/Array_Min_Jumps.ts
+++ array/Array_Min_Jumps.ts
@@ -76,6 +76,6 @@ function ArrayMinJumps( array: number[] ): number {
         return min_array_jumps;
 }
    
-// keep this function call here 
+// KEEP THIS FUNCTION CALL HERE
 // @ts-ignore
 console.log(ArrayMinJumps(readline()));
