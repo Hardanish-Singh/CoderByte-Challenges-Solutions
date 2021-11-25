21cc048c564a5382410e89e1cf4a09538e5fdfdd
Hardanish Singh <hardanishsingh1@gmail.com>
2021-11-25 06:51:01 -0500

    Simple Evens

---
+++ math_fundamentals/Simple_Even.ts
@@ -0,0 +1,31 @@
+/****************************************************************
+ *              CODERBYTE SIMPLE EVENS CHALLENGE                *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function SimpleEvens(num) check whether every single*
+ * number in the passed in parameter is even. If so, return the * 
+ * string true, otherwise return the string false. For example: *
+ * if num is 4602225 your program should return the string      *
+ * false because 5 is not an even number.                       *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: 2222220222 		                                *
+ * Output 1: true                                               *
+ *                                                              *
+ * Input 2: 20864646452                                         *
+ * Output 2: false                                              *
+ *                                                              *
+ ***************************************************************/
+
+function SimpleEvens( num: number ): boolean {
+        var n: string = num.toString();
+        for( let i = 0; i < n.length; i++ ) {
+                if( +n[i] % 2 !== 0 ) {
+                        return false;
+                }
+        }
+        return true;
+}   
+// keep this function call here 
+// @ts-ignore
+console.log(SimpleEvens(readline()));
