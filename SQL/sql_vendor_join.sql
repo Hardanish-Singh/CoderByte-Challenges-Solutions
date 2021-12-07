15b5fbf35eb065424f6b021ba2fc835a3b6239eb
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-07 11:46:05 -0500

    SQL Vendor Join

--- SQL/sql_vendor_join.sql
+++ SQL/sql_vendor_join.sql
@@ -1,3 +1,28 @@
+/***************************************************************************************
+ *                            CODERBYTE SQL VENDOR JOIN CHALLENGE                      *
+ * In this MySQL challenge, your query should return the vendor information along with *
+ * the values from the table cb_vendorinformation. You should combine the values of the*
+ * two tables based on the GroupID column. The final query should only print out the   *
+ * GroupID, CompanyName, and final count of all rows that are grouped into each company*
+ * name under a column titled Count. The output table should be then sorted by the     *
+ * Count column and then sorted by GroupID so that a higher number appears first.      *
+ *                                                                                     *
+ * Your output should look like the following table.                                   *
+ *                                                                                     *
+ *                           +---------+---------------------+-------+                 *
+ *                           | GroupID | CompanyName         | Count |                 *
+ *                           +---------+---------------------+-------+                 *
+ *                           |       27| Machinx             |      1|                 *
+ *                           |        5| WaterBus Enterprise |      1|                 *
+ *                           |       36| Johnson and Sons    |      2|                 * 
+ *                           |       35| Shipping & Co.      |      3|                 *
+ *                           |        6| Alloy LLC           |      3|                 *
+ *                           |       40| FireConsulting      |      5|                 *
+ *                           |       39| News Corp.          |      6|                 *
+ *                           +---------+---------------------+-------+                 *
+ *                                                                                     *
+ **************************************************************************************/
+
 SELECT 
     maintable_V47CA.GroupID,
     cb_vendorinformation.CompanyName,
