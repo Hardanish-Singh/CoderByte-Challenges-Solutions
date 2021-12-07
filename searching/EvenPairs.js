3d59259cebd35358c206d9f2c86d30a97fa2595e
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-07 09:53:14 -0500

    Even Pairs

--- searching/EvenPairs.js
+++ searching/EvenPairs.js
@@ -1,16 +1,25 @@
-/*
-Have the function EvenPairs(str) take the str parameter being passed and determine if a pair of adjacent even numbers exists anywhere in the string. 
-If a pair exists, return the string true, otherwise return false. 
-For example: if str is "f178svg3k19k46" then there are two even numbers at the end of the string, "46" so your program should return the string true. 
-Another example: if str is "7r5gg812" then the pair is "812" (8 and 12) so your program should return the string true.
+/****************************************************************
+ *               CODERBYTE EVEN PAIRS CHALLENGE		        *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function EvenPairs(str) take the str parameter being*
+ * passed & determine if a pair of adjacent even numbers exists *
+ * anywhere in the string. If a pair exists, return the string  *
+ * true, otherwise return false. For example: if str is         *
+ * "f178svg3k19k46" then there are two even numbers at the end  *
+ * of the string, "46" so your program should return the string *
+ * true. Another example: if str is "7r5gg812" then the pair is *
+ * "812" (8 & 12) so your program should return the string true *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "3gy41d216"                                         *
+ * Output 1: true                                               *
+ *                                                              *
+ * Input 2: "f09r27i8e67"                                       *
+ * Output 2: false                                              *
+ *                                                              *
+ ***************************************************************/
 
-Examples
-
-Input: "3gy41d216" 
-Output: true
-Input: "f09r27i8e67" 
-Output: false
-*/
 function EvenPairs( str ) {
         // SOLUTION 1
         for( let i=0; i<str.length; i++ ) {
@@ -48,4 +57,4 @@ function EvenPairs( str ) {
 }
          
 // KEEP THIS FUNCTION CALL HERE 
-console.log(EvenPairs(readline()));
+console.log( EvenPairs( readline() ) );
