76391213409597d12e3e586787cf78092002b1dd
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-08 19:42:29 -0500

    Command Line

--- string manipulation/command_line.js
+++ string manipulation/command_line.js
@@ -4,7 +4,7 @@
  * Problem Statement                                            *
  * Have the function CommandLine(str) take the str parameter    *
  * being passed which represents the parameters given to a      *
- * command in an old PDP system. The parameters are alphanumeric* 
+ * command in an old PDP system. The parameters are alphanumeric*
  * tokens (without spaces) followed by an equal sign & by their *
  * corresponding value. Multiple parameters/value pairs can be  *
  * placed on the command line with a single space between each  *
@@ -13,13 +13,13 @@
  * to isolate the parameters and values to return a list of     *
  * parameter and value lengths. It must provide its result in   *
  * the same format and in the same order by replacing each entry*
- * (tokens and values) by its corresponding length.             *                              
+ * (tokens and values) by its corresponding length.             *
  *                                                              *
  * For example, if str is: "SampleNumber=3234 provider=Dr. M.   *
  * Welby patient=John Smith priority=High" then your function   *
  * should return the string "12=4 8=12 7=10 8=4" because        *
  * "SampleNumber" is a 12 character token with a 4 character    *
- * value ("3234") followed by "provider" which is an 8 character* 
+ * value ("3234") followed by "provider" which is an 8 character*
  * token followed by a 12 character value ("Dr. M. Welby"), etc.*
  *                                                              *
  * Examples                                                     *
