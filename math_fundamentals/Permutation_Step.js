9f6c3736c9555ff39930c7ca488495b17f083625
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-09 13:53:03 -0500

    Permutation Step

--- math_fundamentals/Permutation_Step.js
+++ math_fundamentals/Permutation_Step.js
@@ -1,9 +1,29 @@
-/*
-1. For input 897654321 the output was incorrect. The correct output is 912345678
-2. For input 44444444 the output was incorrect. The correct output is -1
-3. For input 76666666 the output was incorrect. The correct output is -1
-*/
-   
+/****************************************************************
+ *             CODERBYTE PERMUTATION STEP CHALLENGE             *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function PermutationStep(num) take the num parameter*
+ * being passed & return the next number greater than num using *
+ * the same digits.                                             *
+ *                                                              *
+ * For example: if num is 123 return 132, if it's 12453 return  *
+ * 12534. If a number has no greater permutations,              *
+ * return -1 (ie. 999).                                         *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: 11121		                                *
+ * Output 1: 11211                                              *
+ *                                                              *
+ * Input 2: 41352                                               *
+ * Output 2: 41523                                              *
+ *                                                              *
+ * Input 3: 897654321                                           *
+ * Output 3: 912345678                                          *
+ *                                                              *
+ * Input 4: 76666666                                            *
+ * Output 4: -1                                                 *
+ *                                                              *
+ ***************************************************************/
 
 function swap_index_values( leftIndex, rightIndex, nums ) {
         [ nums[rightIndex], nums[leftIndex] ]  = [ nums[leftIndex], nums[rightIndex] ];   
