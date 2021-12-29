362b443fe22ad7a723c886fcd3561c9bdf560a83
Hardanish Singh <hardanishsingh1@gmail.com>
2021-12-29 11:21:03 -0500

    Spiral Matrix

--- array/Matrix_Spiral.js
+++ array/Matrix_Spiral.js
@@ -1,3 +1,31 @@
+/****************************************************************
+ *                 CODERBYTE MATRIX SPIRAL CHALLENGE            *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function MatrixSpiral(strArr) read the array of     *
+ * strings stored in strArr which will represent a 2D N matrix, *
+ * and your program should return the elements after printing   *
+ * them in a clockwise, spiral order. You should return the     *
+ * newly formed list of elements as a string with the numbers   *
+ * separated by commas.                                         *
+ *                                                              *
+ * For example: strArr is "[1, 2, 3]", "[4, 5, 6]", "[7, 8, 9]" *
+ * then this looks like the following 2D matrix:                *
+ *                              1 2 3                           *
+ *                              4 5 6                           *
+ *                              7 8 9                           *
+ * So your program should return the elements of this matrix in *
+ * a clockwise, spiral order which is: 1,2,3,6,9,8,7,4,5        *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: ["[1, 2]", "[10, 14]"]                              *
+ * Output 1: 1,2,14,10                                          *
+ *                                                              *
+ * Input 2: ["[4, 5, 6, 5]", "[1, 1, 2, 2]", "[5, 4, 2, 9]"]    *
+ * Output 2: 4,5,6,5,2,9,2,4,5,1,1,2                            *
+ *                                                              *
+ ***************************************************************/
+
 function moveRight( matrix, i, j, sprialOrder ) {
         while( true ) {
                 j++;
