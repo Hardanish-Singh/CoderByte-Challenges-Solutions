0ad6ce47ca6ea3cfd55b1e04b97cea67b4893e97
Hardanish Singh <hardanishsingh1@gmail.com>
2022-01-01 10:30:34 -0500

    Missing Digit

--- math_fundamentals/Missing_Digit.js
+++ math_fundamentals/Missing_Digit.js
@@ -1,3 +1,31 @@
+/****************************************************************
+ *              CODERBYTE MISSING DIGIT CHALLENGE               *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function MissingDigit(str) take the str parameter,	*
+ * which will be simple mathematical formula with three numbers,*
+ * a single operator (+, -, *, or /) and an equal sign (=) and  *
+ * return the digit that completes the equation.                *
+ *                                                              *
+ * In one of the numbers in the equation, there will be an x    * 
+ * character, and your program should determine what digit is   *
+ * missing.                                                     *
+ *                                                              *
+ * For example, if str is "3x + 12 = 46" then your program      *
+ * should output 4. The x character can appear in any of the    * 
+ * three numbers and all three numbers will be greater than or  *
+ * equal to 0 and less than or equal to 1000000.                *
+ *                                                              *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: "4 - 2 = x"		                                *
+ * Output 1: 2                                                  *
+ *                                                              *
+ * Input 2: "1x0 * 12 = 1200"		                        *
+ * Output 2: 0                                                  *
+ *                                                              *
+ ***************************************************************/
+
 function parse( str ) {
         return Function(`'use strict'; return (${str})`)()
 }
