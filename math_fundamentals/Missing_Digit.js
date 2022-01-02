d397ecb95ca24bbe28053e879dac6264be17e51d
Hardanish Singh <hardanishsingh1@gmail.com>
2022-01-02 10:03:00 -0500

    Missing Digit

--- math_fundamentals/Missing_Digit.js
+++ math_fundamentals/Missing_Digit.js
@@ -7,12 +7,12 @@
  * a single operator (+, -, *, or /) and an equal sign (=) and  *
  * return the digit that completes the equation.                *
  *                                                              *
- * In one of the numbers in the equation, there will be an x    * 
+ * In one of the numbers in the equation, there will be an x    *
  * character, and your program should determine what digit is   *
  * missing.                                                     *
  *                                                              *
  * For example, if str is "3x + 12 = 46" then your program      *
- * should output 4. The x character can appear in any of the    * 
+ * should output 4. The x character can appear in any of the    *
  * three numbers and all three numbers will be greater than or  *
  * equal to 0 and less than or equal to 1000000.                *
  *                                                              *
@@ -60,7 +60,7 @@ function MissingDigit( str ) {
                                 equation += missing_digit_equation[k];
                         }
                 }
-                equation.trim();
+                equation = equation.trim();
                 if( equation.startsWith("0") && equation.length > 1 ) {
                         i++;
                         continue;
