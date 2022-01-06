6213f2aac9d0dbbd2db2e90530d73140ee77f456
Hardanish Singh <hardanishsingh1@gmail.com>
2022-01-06 09:38:17 -0500

    SQL Company Boss

--- SQL/SQL_Company_Boss.sql
+++ SQL/SQL_Company_Boss.sql
@@ -1,3 +1,27 @@
+/*****************************************************************************************************************
+*                                               SQL COMPANY BOSS                                                 *
+*                                                                                                                *
+*        Your table: maintable_WD9RV                                                                             *
+*        MySQL version: 5.5.56-log                                                                               *
+*                                                                                                                *
+*        In this MySQL challenge, your query should return all the people who report to either Jenny Richards    *
+*        or have a NULL value in ReportsTo. The rows should be ordered by Age. Your query should also add a      *
+*        column at the end with a title of Boss Title where the value is either None or CEO.                     *
+*                                                                                                                *
+*        +-----------+----------+----------------+---------------+-----+-----------+                             *
+*        | FirstName | LastName | ReportsTo      | Position      | Age | BossTitle |                             *
+*        +-----------+----------+----------------+---------------+-----+-----------+                             *
+*        | Ashley    | Johnson  | null           | Intern        | 25  | None      |                             *
+*        +-----------+----------+----------------+---------------+-----+-----------+                             *
+*        | Noah      | Fritz    | Jenny Richards | Assistant	 | 30  | CEO       |                             *
+*        +-----------+----------+----------------+---------------+-----+-----------+                             *
+*        | David     | S        | Jenny Richards | Director	 | 32  | CEO       |                             *
+*        +-----------+----------+----------------+---------------+-----+-----------+                             *
+*        | Jenny     | Richards | null           | CEO           | 45  | None      |                             *
+*        +-----------+----------+----------------+---------------+-----+-----------+                             *
+*                                                                                                                *
+*****************************************************************************************************************/
+
 SELECT 
     FirstName,
     LastName,
