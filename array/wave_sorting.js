6919bda54e2ef2c10fccd4062fba36d8106a0503
Hardanish Singh <hardanishsingh1@gmail.com>
2022-01-23 20:40:01 -0500

    Wave Sorting

--- array/wave_sorting.js
+++ array/wave_sorting.js
@@ -1,8 +1,36 @@
-function waveSort(arr) {
-    arr.sort(function(a, b){return a - b});
-    for(let i=0; i<arr.length-1; i+=2){
-        let temp = arr[i]; 
-        arr[i] = arr[i+1]; 
-        arr[i+1] = temp; 
+/************************************************************************
+ *                 CODERBYTE WAVE SORTING CHALLENGE                     *
+ *                                                                      *
+ * Problem Statement                                                    *
+ * Have the function WaveSorting(arr) take the array of positive integer*
+ * stored in arr and return the string true if the numbers can be       *
+ * arranged in a wave pattern: a1 > a2 < a3 > a4 < a5 > ..., otherwise  *
+ * return the string false. For example, if arr is: [0, 1, 2, 4, 1, 4], *
+ * then a possible wave ordering of the numbers is: [2, 0, 4, 1, 4, 1]. *
+ * So for this input your program should return the string true.        *
+ * The input array will always contain at least 2 elements.             *
+ * More examples are given below as sample test cases.                  *
+ *                                                                      *
+ * Examples                                                             *
+ * Input 1: [0, 1, 2, 4, 1, 1, 1]                                       *
+ * Output 1: false                                                      *
+ *                                                                      *
+ * Input 2: [0, 4, 22, 4, 14, 4, 2]                                     *
+ * Output 2: true                                                       *
+ *                                                                      *
+ ***********************************************************************/
+
+function WaveSorting(arr) { 
+    arr = arr.sort( (a, b) => a - b );
+    let smalls = arr.slice(0, Math.floor(arr.length / 2)); 
+    let bigs = arr.slice(Math.floor(arr.length / 2), arr.length);
+    for (let i = 0; i < smalls.length; i++) {
+        if (smalls[i] >= bigs[i]) {
+            return 'false';
+        }
     }
+    return 'true';
 }
+
+// KEEP THIS FUNCTION CALL HERE
+console.log(WaveSorting(readline()));
