5f7aa3c9fbb1af0c132f7b255848d48322cd73e6
Pranay Kumar <pranaykumarmolleti@gmail.com>
2023-12-10 16:02:49 +0530

    Additive_Persistence.js updated

--- array/Additive_Persistence.js
+++ array/Additive_Persistence.js
@@ -19,25 +19,23 @@
  *                                                              *
  ***************************************************************/
 
- function AdditivePersistence(num) {
-        num = String( num );
-        if( num.length === 1 ) {
-                return 0;
+function AdditivePersistence(num) {
+        if (num < 10) {
+            return 0;
         }
+    
         let count = 0;
-        while( num.length != 1 ) {
-                let sum = 0;
-                for( let i=0; i<num.length; i++ ) {
-                        sum += Number(num[i]);
-                }
-                count++;
-                sum = String(sum);
-                if( sum.length === 1 ) {
-                        return count;
-                }
-                num = sum;
+    
+        while (num >= 10) {
+            num = String(num)
+                .split('')
+                .map(Number)
+                .reduce((a, b) => a + b);
+            count++;
         }
-}
-         
+    
+        return count;
+    }
+    
 // KEEP THIS FUNCTION CALL HERE
-console.log( AdditivePersistence( readline() ) );
+console.log(AdditivePersistence(2718))
