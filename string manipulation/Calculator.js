c1fadb8153c472a414c059d29cfcb56342faa716
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-11 10:46:19 -0500

    Calculator

--- string manipulation/Calculator.js
+++ string manipulation/Calculator.js
@@ -2,7 +2,7 @@
  *             CODERBYTE CALCULATOR CHALLENGE                   *
  *                                                              *
  * Problem Statement                                            *
- * Have the function Calculator(str) take the str parameter     * 
+ * Have the function Calculator(str) take the str parameter     *
  * being passed and evaluate the mathematical expression within *
  * in. For example, if str were "2+(3-1)*3" the output should   *
  * be 8. Another example: if str were "(2-0)(6/2)" the output   *
@@ -26,46 +26,39 @@
  *                                                              *
  ***************************************************************/
 
-function parse( str ) {
-    return Function(`'use strict'; return (${str})`)()
+function parse(str) {
+    return Function(`'use strict'; return (${str})`)();
 }
 
-function Calculator(string) { 
+function Calculator(string) {
     let evaluateExpression = new String("");
     let splittedString = string.split("");
     let flag = false;
-    for( let i=0; i<splittedString.length; i++ ) {
-        if( i != 0 && i!=splittedString.length - 1 ) {
-            if(
-                splittedString[i] == ')' 
-                    && 
-                ( splittedString[i+1] == Number(splittedString[i+1]) || splittedString[i+1] == '(' ) 
-                    && 
+    for (let i = 0; i < splittedString.length; i++) {
+        if (i != 0 && i != splittedString.length - 1) {
+            if (
+                splittedString[i] == ")" &&
+                (splittedString[i + 1] == Number(splittedString[i + 1]) || splittedString[i + 1] == "(") &&
                 flag == false
             ) {
                 evaluateExpression += splittedString[i] + "*";
                 flag = true;
-            }
-            else if(
-                    splittedString[i] == '(' 
-                        && 
-                    ( splittedString[i-1] == Number(splittedString[i-1]) || splittedString[i-1] == ')' ) 
-                        && 
-                    flag == false 
-                ) {
+            } else if (
+                splittedString[i] == "(" &&
+                (splittedString[i - 1] == Number(splittedString[i - 1]) || splittedString[i - 1] == ")") &&
+                flag == false
+            ) {
                 evaluateExpression += "*" + splittedString[i];
                 flag = true;
+            } else {
+                evaluateExpression += splittedString[i];
             }
-            else {
-                evaluateExpression += splittedString[i]; 
-            } 
-        }
-        else {
+        } else {
             evaluateExpression += splittedString[i];
         }
-    }	
-    return parse( evaluateExpression );
+    }
+    return parse(evaluateExpression);
 }
-     
+
 // KEEP THIS FUNCTION CALL HERE
-console.log( Calculator( readline() ) );
+console.log(Calculator(readline()));
