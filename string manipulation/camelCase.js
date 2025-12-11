70413445bdf5137b7f9e4a76f817600ecf28a805
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-11 14:12:43 -0500

    Camel Case

--- string manipulation/camelCase.js
+++ string manipulation/camelCase.js
@@ -21,37 +21,38 @@
  ***************************************************************/
 
 function CamelCase(str) {
-  let string = str;
-  let camelCaseString = new String("");
-  let camelCaseArray = new Array();
-  for (let i = 0; i < string.length; i++) if (!(string[i].toLowerCase() >= "a" && string[i].toLowerCase() <= "z")) camelCaseArray.push(i);
-  if (camelCaseArray.length === 0) return string;
-  let position = 0;
-  let toggleFlag = 0;
-  for (let i = 0; i < camelCaseArray.length; i++) {
-    toggleFlag = 0;
-    for (let j = position; j < string.length; j++) {
-      if (j == camelCaseArray[i]) {
-        position++;
-        break;
-      }
-      if (i == 0) camelCaseString += string[j].toLowerCase();
-      else {
-        if (toggleFlag == 0) camelCaseString += string[j].toUpperCase();
-        else camelCaseString += string[j].toLowerCase();
-      }
-      position++;
-      toggleFlag++;
+    let string = str;
+    let camelCaseString = new String("");
+    let camelCaseArray = new Array();
+    for (let i = 0; i < string.length; i++)
+        if (!(string[i].toLowerCase() >= "a" && string[i].toLowerCase() <= "z")) camelCaseArray.push(i);
+    if (camelCaseArray.length === 0) return string;
+    let position = 0;
+    let toggleFlag = 0;
+    for (let i = 0; i < camelCaseArray.length; i++) {
+        toggleFlag = 0;
+        for (let j = position; j < string.length; j++) {
+            if (j == camelCaseArray[i]) {
+                position++;
+                break;
+            }
+            if (i == 0) camelCaseString += string[j].toLowerCase();
+            else {
+                if (toggleFlag == 0) camelCaseString += string[j].toUpperCase();
+                else camelCaseString += string[j].toLowerCase();
+            }
+            position++;
+            toggleFlag++;
+        }
     }
-  }
-  toggleFlag = 0;
-  for (let k = position; k < string.length; k++) {
-    if (toggleFlag == 0) camelCaseString += string[k].toUpperCase();
-    else camelCaseString += string[k].toLowerCase();
-    position++;
-    toggleFlag++;
-  }
-  return camelCaseString;
+    toggleFlag = 0;
+    for (let k = position; k < string.length; k++) {
+        if (toggleFlag == 0) camelCaseString += string[k].toUpperCase();
+        else camelCaseString += string[k].toLowerCase();
+        position++;
+        toggleFlag++;
+    }
+    return camelCaseString;
 }
 
 // KEEP THIS FUNCTION CALL HERE
