4115453b8a58ae4b517ed1647e3fe78431218a13
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-11 10:40:39 -0500

    AB Check

--- string manipulation/ab_check.js
+++ string manipulation/ab_check.js
@@ -21,13 +21,12 @@
 function ABCheck(string) {
     let ab_counter = 0;
     let temp = new String("");
-    for(let i=0; i<string.length; i++){
-        if(string[i] === 'a' || string[i] === 'b') {
-            if(temp.length > 0 && string[i] !== temp && ab_counter === 3) return true;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "a" || string[i] === "b") {
+            if (temp.length > 0 && string[i] !== temp && ab_counter === 3) return true;
             ab_counter = 0;
             temp = string[i];
-        }
-        else ab_counter++;
+        } else ab_counter++;
     }
     return false;
 }
