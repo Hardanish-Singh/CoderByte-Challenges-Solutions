6083aa6180a6fbe25f2c70d4c93624a8166d45cf
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-12 15:13:27 -0500

    Counting Minutes

--- string manipulation/counting_minutes.js
+++ string manipulation/counting_minutes.js
@@ -7,7 +7,7 @@
  * (each properly formatted with a colon and am or pm)          *
  * separated by a hyphen and return the total number of minutes *
  * between the two times. The time will be in a 12 hour clock   *
- * format.                                                      * 
+ * format.                                                      *
  * For example: if str is 9:00am-10:00am then the               *
  * output should be 60. If str is 1:00pm-11:00am the output     *
  * should be 1320.                                              *
@@ -34,23 +34,22 @@ function CountingMinutes(dateString) {
     let minutes2 = "";
     let ampm2 = "";
     for (let i = 0; i < splitDateStringAtHiphen.length; i++) {
-      let splitDateStringAtColon = splitDateStringAtHiphen[i].split(":");
-      for (let j = 0; j < splitDateStringAtColon.length; j++) {
-        if (j == 0 && i == 0) hours1 = Number(splitDateStringAtColon[j]);
-        else if (j == 0 && i == 1) hours2 = Number(splitDateStringAtColon[j]);
-        else if (j == 1 && i == 0) {
-          for (let k = 0; k < splitDateStringAtColon[j].length; k++) {
-            if (k < 2) minutes1 += splitDateStringAtColon[j][k];
-            else ampm1 += splitDateStringAtColon[j][k];
-          }
-        } 
-        else if (j == 1 && i == 1) {
-          for (let k = 0; k < splitDateStringAtColon[j].length; k++) {
-            if (k < 2) minutes2 += splitDateStringAtColon[j][k];
-            else ampm2 += splitDateStringAtColon[j][k];
-          }
+        let splitDateStringAtColon = splitDateStringAtHiphen[i].split(":");
+        for (let j = 0; j < splitDateStringAtColon.length; j++) {
+            if (j == 0 && i == 0) hours1 = Number(splitDateStringAtColon[j]);
+            else if (j == 0 && i == 1) hours2 = Number(splitDateStringAtColon[j]);
+            else if (j == 1 && i == 0) {
+                for (let k = 0; k < splitDateStringAtColon[j].length; k++) {
+                    if (k < 2) minutes1 += splitDateStringAtColon[j][k];
+                    else ampm1 += splitDateStringAtColon[j][k];
+                }
+            } else if (j == 1 && i == 1) {
+                for (let k = 0; k < splitDateStringAtColon[j].length; k++) {
+                    if (k < 2) minutes2 += splitDateStringAtColon[j][k];
+                    else ampm2 += splitDateStringAtColon[j][k];
+                }
+            }
         }
-      }
     }
     minutes1 = Number(minutes1);
     minutes2 = Number(minutes2);
@@ -61,17 +60,16 @@ function CountingMinutes(dateString) {
     let minutesOneConverted = hours1 * 60 + minutes1;
     let minutesTwoConverted = hours2 * 60 + minutes2;
     if (minutesOneConverted > minutesTwoConverted) {
-      hours2 += 24;
-      let timeOne = hours2 * 60 + minutes2;
-      let timeTwo = hours1 * 60 + minutes1;
-      return timeOne - timeTwo;
+        hours2 += 24;
+        let timeOne = hours2 * 60 + minutes2;
+        let timeTwo = hours1 * 60 + minutes1;
+        return timeOne - timeTwo;
     } else {
-      let timeOne = hours2 * 60 + minutes2;
-      let timeTwo = hours1 * 60 + minutes1;
-      return timeOne - timeTwo;
+        let timeOne = hours2 * 60 + minutes2;
+        let timeTwo = hours1 * 60 + minutes1;
+        return timeOne - timeTwo;
     }
-  }
-  
-  // KEEP THIS FUNCTION CALL HERE
-  console.log(CountingMinutes(readline()));
-  
+}
+
+// KEEP THIS FUNCTION CALL HERE
+console.log(CountingMinutes(readline()));
