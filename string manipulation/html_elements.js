d6de03c288b2e40f24dd6e0e6f7a198513f231d0
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-13 14:35:28 -0500

    HTML ELEMENTS

--- string manipulation/html_elements.js
+++ string manipulation/html_elements.js
@@ -30,7 +30,7 @@
  * Output 2: i                                                   *
  *                                                               *
  * Solution Efficiency                                           *
- * This user scored higher than 84.5% of users who solved this   * 
+ * This user scored higher than 84.5% of users who solved this   *
  * challenge.                                                    *
  *                                                               *
  ****************************************************************/
@@ -42,17 +42,20 @@ function HTMLElements(string) {
     let closingTags = new Array();
     let temp1 = new String("");
     let temp2 = new String("");
-    for(let i=0; i<htmlFindStringArray.length; i++){
-        if(htmlFindStringArray[i].startsWith("</")) closingTags.push(htmlFindStringArray[i]);
+    for (let i = 0; i < htmlFindStringArray.length; i++) {
+        if (htmlFindStringArray[i].startsWith("</")) closingTags.push(htmlFindStringArray[i]);
         else openingTags.push(htmlFindStringArray[i]);
     }
-    for(let i=0; i<openingTags.length; i++){
+    for (let i = 0; i < openingTags.length; i++) {
         temp1 = new String("");
-        for(let j=0; j<openingTags[i].length; j++) if(openingTags[i][j]!=='<' && openingTags[i][j]!=='>') temp1 += openingTags[i][j];
-        for(let m=0; m<closingTags.length; m++) {
+        for (let j = 0; j < openingTags[i].length; j++)
+            if (openingTags[i][j] !== "<" && openingTags[i][j] !== ">") temp1 += openingTags[i][j];
+        for (let m = 0; m < closingTags.length; m++) {
             temp2 = new String("");
-            for(let n=0; n<closingTags[m].length; n++) if(closingTags[m][n]!=='<' &&closingTags[m][n]!=="/" && closingTags[m][n]!=='>') temp2 += closingTags[m][n];
-            if(temp1===temp2) {
+            for (let n = 0; n < closingTags[m].length; n++)
+                if (closingTags[m][n] !== "<" && closingTags[m][n] !== "/" && closingTags[m][n] !== ">")
+                    temp2 += closingTags[m][n];
+            if (temp1 === temp2) {
                 openingTags.splice(i, 1);
                 i--;
                 closingTags.splice(m, 1);
@@ -60,8 +63,10 @@ function HTMLElements(string) {
             }
         }
     }
-    return (openingTags.length === 0 ) ? true : openingTags[openingTags.length-1].slice(1).slice(0, openingTags[0].length-2);
-  }
-     
-  // KEEP THIS FUNCTION CALL HERE
-  console.log(HTMLElements(readline()));
+    return openingTags.length === 0
+        ? true
+        : openingTags[openingTags.length - 1].slice(1).slice(0, openingTags[0].length - 2);
+}
+
+// KEEP THIS FUNCTION CALL HERE
+console.log(HTMLElements(readline()));
