e4ff36a9c826a52a06844e61db077658da4f5f1f
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-13 14:34:36 -0500

    FizzBuzz

--- string manipulation/fizzbuzz.js
+++ string manipulation/fizzbuzz.js
@@ -25,21 +25,21 @@
  * Output 2: "1 2 Fizz 4 Buzz Fizz 7 8"                         *
  *                                                              *
  * Solution Efficiency                                          *
- * This user scored higher than 62.1% of users who solved this  * 
+ * This user scored higher than 62.1% of users who solved this  *
  * challenge.                                                   *
  *                                                              *
  ***************************************************************/
 
-function FizzBuzz(num) { 
+function FizzBuzz(num) {
     let string = new String("");
-    for(let i=1; i<=num; i++){
-        if(i%3===0 && i%5===0) string += "FizzBuzz ";
-        else if(i%3===0) string += "Fizz ";
-        else if(i%5===0) string += "Buzz ";
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) string += "FizzBuzz ";
+        else if (i % 3 === 0) string += "Fizz ";
+        else if (i % 5 === 0) string += "Buzz ";
         else string += i + " ";
     }
     return string.trim();
 }
-    
+
 // KEEP THIS FUNCTION CALL HERE
 console.log(FizzBuzz(readline()));
