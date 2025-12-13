dcf0ad473fd5ec56701b836b2576c8a7e8451d5b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-13 14:34:49 -0500

    Formatted Division

--- string manipulation/formatted_division.js
+++ string manipulation/formatted_division.js
@@ -5,7 +5,7 @@
  * Have the function FormattedDivision(num1,num2) take both     *
  * parameters being passed, divide num1 by num2, and return the *
  * result as a string with properly formatted commas and 4      *
- * significant digits after the decimal place.                  * 
+ * significant digits after the decimal place.                  *
  *                                                              *
  * For example: if num1 is 123456789 and num2 is 10000          *
  * the output should be "12,345.6789".                          *
@@ -20,15 +20,15 @@
  * Output 2: 1.0000                                             *
  *                                                              *
  * Solution Efficiency                                          *
- * This user scored higher than 53.8% of users who solved this  * 
+ * This user scored higher than 53.8% of users who solved this  *
  * challenge.                                                   *
  *                                                              *
  ***************************************************************/
 
-function FormattedDivision(num1,num2) { 
-    let array = ((num1/num2).toFixed(4)).split(".");
+function FormattedDivision(num1, num2) {
+    let array = (num1 / num2).toFixed(4).split(".");
     return new String(new Intl.NumberFormat().format(array[0]) + "." + array[1]).toString();
 }
-    
+
 // KEEP THIS FUNCTION CALL HERE
 console.log(FormattedDivision(readline()));
