d3c411788835118d9e9e895a5720224ca2fc2a58
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-15 09:52:47 -0500

    Simple Password

--- string manipulation/simple_password.js
+++ string manipulation/simple_password.js
@@ -9,7 +9,7 @@
  * 2. It must contain at least one number.                      *
  * 3. It must contain a punctuation mark.                       *
  * 4. It cannot have the word "password" in the string.         *
- * 5. It must be longer than 7 characters and                   * 
+ * 5. It must be longer than 7 characters and                   *
  *    shorter than 31 characters.                               *
  * If all the above constraints are met within the string, the  *
  * your program should return the string true, otherwise your   *
@@ -26,15 +26,15 @@
  *                                                              *
  ***************************************************************/
 
-function SimplePassword(str) { 
-        finalString = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
-        if(str.length === finalString.length) return false;
-        if(str.match(/[a-zA-Z]/g).length === 0 || str.match(/[0-9]/g).length === 0) return false;
-        str = str.toLowerCase();
-        if(str.includes("password")) return false;
-        if(str.length <= 7 || str.length >= 31) return false;
-        return true;
+function SimplePassword(str) {
+    finalString = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
+    if (str.length === finalString.length) return false;
+    if (str.match(/[a-zA-Z]/g).length === 0 || str.match(/[0-9]/g).length === 0) return false;
+    str = str.toLowerCase();
+    if (str.includes("password")) return false;
+    if (str.length <= 7 || str.length >= 31) return false;
+    return true;
 }
-        
+
 // KEEP THIS FUNCTION CALL HERE
 console.log(SimplePassword(readline()));
