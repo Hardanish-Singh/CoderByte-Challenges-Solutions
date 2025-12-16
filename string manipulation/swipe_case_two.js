31312b5233f535e3198ee43e6dd7451baf70ea51
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-16 10:57:34 -0500

    Swipe Case Two

--- string manipulation/swipe_case_two.js
+++ string manipulation/swipe_case_two.js
@@ -28,26 +28,27 @@ function SwapII(str) {
     let swipeCaseTwo = new String("");
     let start = 0;
     let end = 0;
-    for(let i=0; i<array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         start = 0;
         end = 0;
-        if(array[i].length === 1) {
+        if (array[i].length === 1) {
             if (array[i] == array[i].toLowerCase()) swipeCaseTwo += array[i].toUpperCase();
             else swipeCaseTwo += array[i].toLowerCase();
-        }
-        else {
-            for(let j=0; j<array[i].length; j++){
-                if(Number(array[i][j]) && !Number(array[i][j+1])) {
-                    if(array[i].substr(j+1).match(/\d+/g)) {
-                        swipeCaseTwo += array[i].substr(j+1).match(/\d+/g).join("");
+        } else {
+            for (let j = 0; j < array[i].length; j++) {
+                if (Number(array[i][j]) && !Number(array[i][j + 1])) {
+                    if (array[i].substr(j + 1).match(/\d+/g)) {
+                        swipeCaseTwo += array[i]
+                            .substr(j + 1)
+                            .match(/\d+/g)
+                            .join("");
                         start = j;
-                        end = array[i].substr(j+1).match(/\d+/).index;
-                    }
-                    else if(array[i].substr(start, end).match(/\d+/g)) swipeCaseTwo += array[i].substr(start, end).match(/\d+/g).join("");
+                        end = array[i].substr(j + 1).match(/\d+/).index;
+                    } else if (array[i].substr(start, end).match(/\d+/g))
+                        swipeCaseTwo += array[i].substr(start, end).match(/\d+/g).join("");
                     else swipeCaseTwo += array[i][j];
-                }
-                else {
-                    if(array[i][j] == array[i][j].toLowerCase()) swipeCaseTwo += array[i][j].toUpperCase();
+                } else {
+                    if (array[i][j] == array[i][j].toLowerCase()) swipeCaseTwo += array[i][j].toUpperCase();
                     else swipeCaseTwo += array[i][j].toLowerCase();
                 }
             }
@@ -55,6 +56,6 @@ function SwapII(str) {
     }
     return swipeCaseTwo;
 }
-     
+
 // KEEP THIS FUNCTION CALL HERE
 console.log(SwapII(readline()));
