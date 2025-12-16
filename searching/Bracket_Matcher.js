152cd441c5057221632e25b71affd2a7b9c8d132
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-16 11:09:54 -0500

    Bracket Matcher

--- searching/Bracket_Matcher.js
+++ searching/Bracket_Matcher.js
@@ -1,5 +1,5 @@
 /****************************************************************
- *          CODERBYTE BRACKET MATCHER CHALLENGE		        *
+ *          CODERBYTE BRACKET MATCHER CHALLENGE		        	*
  *                                                              *
  * Problem Statement                                            *
  * Have the function BracketMatcher(str) take the str parameter	*
@@ -9,7 +9,7 @@
  * should be 1, but if str is "((hello (world))" the the output	*
  * should be 0 because the brackets do not correctly match up.	*
  * Only "(" and ")" will be used as brackets. If str contains	*
- * no brackets return 1.    					*
+ * no brackets return 1.    									*
  *                                                              *
  * Examples                                                     *
  * Input 1: "(coder)(byte))"                                    *
@@ -20,26 +20,20 @@
  *                                                              *
  ***************************************************************/
 
-function BracketMatcher( str )
-{
-	let count = 0;
-	for( let i=0; i<str.length; i++ )
-	{
-		if( str[i] === '(' )
-		{
-			count++;
-		}
-		else if( str[i] == ')' )
-		{
-			count--;
-		}
-		if( count == -1 )
-		{
-			return 0;
-		}
-	}
+function BracketMatcher(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "(") {
+            count++;
+        } else if (str[i] == ")") {
+            count--;
+        }
+        if (count == -1) {
+            return 0;
+        }
+    }
 
-	return ( count === 0 ) ? 1 : 0;
+    return count === 0 ? 1 : 0;
 }
 
 // KEEP THIS FUNCTION CALL HERE
