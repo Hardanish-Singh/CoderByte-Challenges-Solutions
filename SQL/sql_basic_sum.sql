c81849f4aebccd25e75ec012bcf27fa121548933
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-16 11:07:25 -0500

    SQL Basic Sum

--- SQL/sql_basic_sum.sql
+++ SQL/sql_basic_sum.sql
@@ -5,7 +5,7 @@
 *        MySQL version: 5.5.56-log                                                                               *
 *                                                                                                                *
 *        In this MySQL challenge,                                                                                *
-*        your query should return LastName & the sum of Age from your table for all users with a LastName = Smith* 
+*        your query should return LastName & the sum of Age from your table for all users with a LastName = Smith*
 *        The column title of the summed ages should be SumAge. Your output should look like the following table. *
 *                                                                                                                *
 *        +----------+---------+                                                                                  *
@@ -15,6 +15,7 @@
 *        +----------+---------+                                                                                  *
 *                                                                                                                *
 *****************************************************************************************************************/
+
 SELECT 
         LastName,
         SUM(Age) AS SumAge
