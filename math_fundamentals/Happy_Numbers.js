7945d650bd393c71446a5a60a58cda55c198065b
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2025-12-17 09:16:57 -0500

    Happy Numbers

--- math_fundamentals/Happy_Numbers.js
+++ math_fundamentals/Happy_Numbers.js
@@ -8,7 +8,7 @@
  * number, otherwise return false.       						*
  *                                                              *
  * For example: the number 10 is Happy because 1^2 + 0^2		*
- * converges to 1.       					*
+ * converges to 1.       										*
  *                                                              *
  * Examples                                                     *
  * Input 1: 1		                                        *
