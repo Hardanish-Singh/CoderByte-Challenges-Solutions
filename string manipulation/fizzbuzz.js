799ecf0e8fdea7773aa8a544e77871e8be76a250
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-01-13 10:05:17 -0500

    Fizz Buzz

--- string manipulation/fizzbuzz.js
+++ string manipulation/fizzbuzz.js
@@ -32,12 +32,14 @@
 
 function FizzBuzz(num) {
     let string = new String("");
+
     for (let i = 1; i <= num; i++) {
         if (i % 3 === 0 && i % 5 === 0) string += "FizzBuzz ";
         else if (i % 3 === 0) string += "Fizz ";
         else if (i % 5 === 0) string += "Buzz ";
         else string += i + " ";
     }
+
     return string.trim();
 }
 
