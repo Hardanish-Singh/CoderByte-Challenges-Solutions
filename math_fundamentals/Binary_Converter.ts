f75fc5fef557df703c6a872b57e02b246013bacf
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-07-11 12:29:17 -0400

    Binary Converter

--- math_fundamentals/Binary_Converter.ts
+++ math_fundamentals/Binary_Converter.ts
@@ -20,11 +20,13 @@
 
 function BinaryConverter(str: string): number {
     let sum: number = 0;
+
     for (let i: number = str.length - 1; i >= 0; i--) {
         if (str[i] === "1") {
             sum += Math.pow(2, str.length - 1 - i);
         }
     }
+
     return sum;
 }
 
