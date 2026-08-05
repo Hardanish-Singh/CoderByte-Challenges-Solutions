ce2f20fe87d457e7f33be389aa61936ee684ba00
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-08-05 10:01:28 -0400

    Array Rotation

--- array/Array_Rotation.js
+++ array/Array_Rotation.js
@@ -25,47 +25,39 @@
  *                                                              *
  ***************************************************************/
 
-/*
-        SOLUTION 1: USES ADDITIONAL ARRAY
-*/
-function ArrayRotation( arr ) { 
+// SOLUTION 1: USES ADDITIONAL ARRAY
+function ArrayRotation(arr) {
+    let startIndex = arr[0];
 
-        let startIndex = arr[0];
+    let array_rotation = [];
 
-        let array_rotation = [];
+    for (let i = startIndex; i < arr.length; i++) {
+        array_rotation.push(arr[i]);
+    }
 
-        for( let i=startIndex; i<arr.length; i++ ) {
-                array_rotation.push( arr[i] );
-        }
+    for (let i = 0; i < startIndex; i++) {
+        array_rotation.push(arr[i]);
+    }
 
-        for( let i=0; i<startIndex; i++ ) {
-                array_rotation.push( arr[i] );
-        }
-
-        return array_rotation.join(""); 
-      
+    return array_rotation.join("");
 }
-         
+
 // KEEP THIS FUNCTION CALL HERE
-console.log( ArrayRotation( readline() ) );
+console.log(ArrayRotation(readline()));
 
-/*
-        SOLUTION 2: DOES NOT USE ADDITIONAL ARRAY
-*/
+// SOLUTION 2: DOES NOT USE ADDITIONAL ARRAY
 
-function ArrayRotation( arr ) { 
+function ArrayRotation(arr) {
+    let startIndex = arr[0];
 
-        let startIndex = arr[0];
-        
-        for( let i=0; i<startIndex; i++ ) {
-                arr.push( arr[i] );
-        }
+    for (let i = 0; i < startIndex; i++) {
+        arr.push(arr[i]);
+    }
 
-        arr = arr.splice(startIndex);
+    arr = arr.splice(startIndex);
 
-        return arr.join("");
-      
+    return arr.join("");
 }
-   
+
 // KEEP THIS FUNCTION CALL HERE
-console.log( ArrayRotation( readline() ) );
+console.log(ArrayRotation(readline()));
