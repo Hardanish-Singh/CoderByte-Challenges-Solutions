bfc98f88a21ead951958aaeaca4650d9632bcf59
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-08-16 09:28:49 -0400

    Max Subarray

--- array/Max_SubArray.js
+++ array/Max_SubArray.js
@@ -19,28 +19,26 @@
  *                                                              *
  ***************************************************************/
 
-function MaxSubarray(nums) { 
-
-        let sum = -100001;
-        let temp = 0;
-        for( let i=0; i<nums.length; i++ ) {
-                temp = nums[i];
-                for( let j=i+1; j<nums.length; j++ ) {
-                        temp += nums[j];
-                        if( temp < 0 ) {
-                                break;
-                        }
-                        if( temp >= sum  ) {
-                                sum = temp;    
-                        }      
-                }
-                if( nums[i] > sum ) {
-                        sum = nums[i];
-                }
+function MaxSubarray(nums) {
+    let sum = -100001;
+    let temp = 0;
+    for (let i = 0; i < nums.length; i++) {
+        temp = nums[i];
+        for (let j = i + 1; j < nums.length; j++) {
+            temp += nums[j];
+            if (temp < 0) {
+                break;
+            }
+            if (temp >= sum) {
+                sum = temp;
+            }
         }
-        return sum;
-      
+        if (nums[i] > sum) {
+            sum = nums[i];
+        }
+    }
+    return sum;
 }
-        
+
 // KEEP THIS FUNCTION CALL HERE
-console.log( MaxSubarray( readline() ) );
+console.log(MaxSubarray(readline()));
