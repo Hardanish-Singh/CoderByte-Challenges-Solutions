486cfe5edce488a3f753bf1649440150124e48f3
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2026-08-19 11:58:02 -0400

    Rectangle Area

--- array/RectangleArea.js
+++ array/RectangleArea.js
@@ -22,23 +22,38 @@
  ***************************************************************/
 
 function RectangleArea(strArray) {
-  let A = strArray[0].slice(1).slice(0, strArray[0].length-2).split(" ");
-  let B = strArray[1].slice(1).slice(0, strArray[1].length-2).split(" ");
-  let C = strArray[2].slice(1).slice(0, strArray[2].length-2).split(" ");
-  let D = strArray[3].slice(1).slice(0, strArray[3].length-2).split(" ");
+    let A = strArray[0]
+        .slice(1)
+        .slice(0, strArray[0].length - 2)
+        .split(" ");
+    let B = strArray[1]
+        .slice(1)
+        .slice(0, strArray[1].length - 2)
+        .split(" ");
+    let C = strArray[2]
+        .slice(1)
+        .slice(0, strArray[2].length - 2)
+        .split(" ");
+    let D = strArray[3]
+        .slice(1)
+        .slice(0, strArray[3].length - 2)
+        .split(" ");
 
-  let x1 = Math.abs(A[0]);
-  let y1 = Math.abs(A[1]);
-  let x2 = Math.abs(B[0]);
-  let y2 = Math.abs(B[1]);
-  let x3 = Math.abs(C[0]);
-  let y3 = Math.abs(C[1]);
-  let x4 = Math.abs(D[0]);
-  let y4 = Math.abs(D[1]);
+    let x1 = Math.abs(A[0]);
+    let y1 = Math.abs(A[1]);
+    let x2 = Math.abs(B[0]);
+    let y2 = Math.abs(B[1]);
+    let x3 = Math.abs(C[0]);
+    let y3 = Math.abs(C[1]);
+    let x4 = Math.abs(D[0]);
+    let y4 = Math.abs(D[1]);
 
-  let array1 = [x1, x2, x3, x4];
-  let array2 = [y1, y2, y3, y4];
-  return (Math.max.apply(Math, array1) - Math.min.apply(Math, array1)) * (Math.max.apply(Math, array2) - Math.min.apply(Math, array2));
+    let array1 = [x1, x2, x3, x4];
+    let array2 = [y1, y2, y3, y4];
+    return (
+        (Math.max.apply(Math, array1) - Math.min.apply(Math, array1)) *
+        (Math.max.apply(Math, array2) - Math.min.apply(Math, array2))
+    );
 }
 
 // KEEP THIS FUNCTION CALL HERE
